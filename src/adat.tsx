b4ed990127c07d7207dbbd32cff7cb2d14edb53b
molnarbea <molnarbeatrix11@gmail.com>
2026-09-18 13:01:02 +0200

    kepek javitva

--- src/adat.tsx
+++ src/adat.tsx
@@ -12,20 +12,20 @@ export const ZENELISTA:ZeneTipus[] = [
         cim: "Shape of You",
         enekes: "Ed Sheeran",
         kiadasiEv: 2017,
-        kep: "/kepek/ed.png"
+        kep: "/zenek/kepek/ed.png"
     },
     {
         id: 2,
         cim: "Blinding Lights",
         enekes: "The Weeknd",
         kiadasiEv: 2019,
-        kep: "/kepek/weeknd.jpg"
+        kep: "/zenek/kepek/weeknd.jpg"
     },
     {
         id: 3,
         cim: "Flowers",
         enekes: "Miley Cyrus",
         kiadasiEv: 2023,
-        kep: "/kepek/miley.jpg"
+        kep: "/zenek/kepek/miley.jpg"
     }
 ];
